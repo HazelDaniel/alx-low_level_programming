fda4deb0ad0d2a891840f2889decc9fc3d2422cb
HazelDaniel <hazeldaniel856@gmail.com>
2023-03-16 10:19:11 -0700

    wrote a c program that  prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (failure)
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
+	return (1);
+}
