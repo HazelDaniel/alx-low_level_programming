5e01b9b671df663400136afacbc33aac1304c548
HazelDaniel <hazeldaniel856@gmail.com>
2023-03-20 23:53:21 -0700

    wrote a c program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "_putchar.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *chars = "_putchar";
+	char term = '\n';
+
+	while (*chars != '\0')
+	{
+		_putchar(*chars);
+		chars++;
+	}
+	_putchar(((int)term));
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+A readme file for the 0x02-functions_nested_loops directory
