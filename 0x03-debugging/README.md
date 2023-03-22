ed79a11cd7af34c19646fbad80bbc66232de8727
HazelDaniel <hazeldaniel856@gmail.com>
2023-03-22 13:37:09 -0700

    wrote all that files in a single commit

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+A readme file for the 0x03-debugging directory 
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_
+#define _MAIN_
+#include <stdio.h>
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+#endif /* !_MAIN_ */
