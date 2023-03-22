85974f09c04e0115a47dc2786a422d754163d407
HazelDaniel <hazeldaniel856@gmail.com>
2023-03-21 18:09:40 -0700

    wrote a c program that   prints the first 98 Fibonacci numbers, starting with 1 and 2

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdint.h>
+
+/**
+* main - The entry point into the program
+* first 98 fibonacci numbers
+* Return: Always Return int
+*/
+
+int main(void)
+{
+	int limit = 98;
+	int iter = 2;
+	char* F98 = "83621143489848422977";
+	char* F97 = "51680708854858323072";
+	char* F96 = "31940434634990099905";
+	char* F95 = "19740274219868223167";
+	char* F94 = "12200160415121876738";
+
+
+	while (iter <= limit)
+	{
+		unsigned long fibon = ((pow(1.61803398875, iter)) - pow(-0.61803398875, iter)) / sqrt(5);
+
+			if (iter <= 98)
+			{
+				if (iter < 94)
+					printf("%lu, ", fibon);
+				else if (iter == 94)
+					printf("%s, ", F94);
+				else if (iter == 95)
+					printf("%s, ", F95);
+				else if (iter == 96)
+					printf("%s, ", F96);
+				else if (iter == 97)
+					printf("%s, ", F97);
+				else
+					printf("%s\n", F98);
+			}
+			iter++;
+		}
+
+	return (0);
+}
