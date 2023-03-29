3c765feec74462887f7196fbbca2a780ce95c647
HazelDaniel <hazeldaniel856@gmail.com>
2023-03-29 11:26:34 -0700

    correction to the 6-cap_string.c file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,6 +11,7 @@ char *cap_string(char *s)
 	int count = 0;
 	char prev;
 	int i;
+	int is_word_start;
 
 	while (s[count] != '\0')
 	{
@@ -21,7 +22,11 @@ char *cap_string(char *s)
 		if (s[i] >= 97 && s[i] <= 122)
 		{
 			prev = s[i - 1];
-			if (i == 0 || (prev >= 9 && prev <= 13) || prev == 32)
+			is_word_start = (i == 0 || (prev >= 9 && prev <= 13) || prev == 32
+			|| prev == 33 || prev == 34 || prev == 40 || prev == 41 ||
+			prev == 46 || prev == 59 || prev == 63 || prev == 123
+			|| prev == 125) ? 1 : 0;
+			if (is_word_start == 1)
 			{
 				s[i] = (int)(s[i]) - 32;
 			}
