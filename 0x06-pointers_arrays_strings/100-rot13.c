033347a3ae6ac7afde1cc006ddddb59f7c11d140
HazelDaniel <hazeldaniel856@gmail.com>
2023-03-29 11:57:59 -0700

    my solution to advanced task 8.

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - a function that encodes a string
+ * using rot13 substitution cypher
+ * @s: a string parameter
+ * Return:  a string
+ **/
+
+char *rot13(char *s)
+{
+	char *keys = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *vals = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int count = 0, j;
+
+	for (; s[count] != '\0'; count++)
+	{
+		for(j = 0; keys[j] != '\0'; j++)
+		{
+			if (s[count] == keys[j])
+			{
+				s[count] = vals[j];
+			}
+		}
+	}
+	return s;
+}
