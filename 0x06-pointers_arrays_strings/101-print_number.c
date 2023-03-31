648ed23357a09c8b43447c169bad46440f593d57
HazelDaniel <hazeldaniel856@gmail.com>
2023-03-30 17:49:57 -0700

    correction to the task .9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,81 +1,24 @@
 #include "main.h"
 
 /**
- * print_number - prints any given number digit wise
- * @n: integer parameter
- * Return: - void
- **/
-
+ * print_number - prints an integer;
+ * @n: integer to be printed;
+ */
 void print_number(int n)
 {
-	int dig_base = 10;
-	int asc_num_base = 48;
-	int dig_least = 0;
+	unsigned int n1;
 
-	if (n >= dig_base)
+	if (n < 0)
 	{
-		if (n < dig_base)
-		{
-			return;
-		}
-		else
-			{
-			print_number(n / dig_base);
-			_putchar((n % dig_base) + asc_num_base);
-		}
-		first_dig(n);
-	}
-	else if (-n > dig_least)
+		n1 = -n;
+		_putchar('-');
+	} else
 	{
-		_putchar((int)'-');
-		print_number(-n);
+		n1 = n;
 	}
-	else
-	{
-		_putchar(n + asc_num_base);
-	}
-}
 
-/**
- * first_dig - prints any given number digit wise
- * @x: integer parameter
- * Return: - integer type
- **/
+	if (n1 / 10)
+		print_number(n1 / 10);
 
-int first_dig(int x)
-{
-	int dig_base = 10;
-	int dig_least = 0;
-	int po = 0, init_x = x;
-
-	while (x / dig_base > dig_least)
-	{
-		po++;
-		x /= dig_base;
-	}
-	return ((init_x / power(dig_base, po)));
-}
-
-/**
- * power - returns the value of an integer
- * raised to the power (another integer)
- * @a: integer parameter
- * @b: integer parameter
- * Return: - integer type
- **/
-
-int power(int a, int b)
-{
-	int even_base = 2, unity = 1;
-	int dig_least = 0;
-	int result = 1;
-
-	while (b > dig_least)
-	{
-		if (b % even_base == unity)
-			result *= a;
-		a *= a;
-		b /= even_base;
-	}
-	return (result);
+	_putchar((n1 % 10) + '0');
 }
