6d67aa81781b5c0fac6b669c01cc423ac84d9db7
HazelDaniel <hazeldaniel856@gmail.com>
2023-03-30 17:52:21 -0700

    correction to the last commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,4 +12,5 @@ char *cap_string(char *);
 char *leet(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+void print_number(int n);
 #endif /* !_MAIN_ */
