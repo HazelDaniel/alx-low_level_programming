5ebcf7025a284f57e3611207a10aa54ec1785ccc
HazelDaniel <hazeldaniel856@gmail.com>
2023-04-05 08:26:09 +0100

    correction to task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,22 @@
 #include "main.h"
 
+/**
+  * _pow_recursion - a function that recursively
+	* calculates the positive powers of integers
+  * @x: parameter of type int .
+  * @y: parameter of type int .
+  * Return: int .
+ */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return -1;
+	{
+		return (-1);
+	}
 	if (y == 0)
-		return 1;
+	{
+		return (1);
+	}
 	y--;
-	return x * _pow_recursion(x, y);
+	return (x * _pow_recursion(x, y));
 }
