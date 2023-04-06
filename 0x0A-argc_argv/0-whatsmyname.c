f013c7574e28651ba78d7b7d5790b0d3dbcce338
HazelDaniel <hazeldaniel856@gmail.com>
2023-04-06 08:40:12 +0100

    submission for task 1.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[], char *envp[])
+/**
+  * main - The entry point of the program
+  * @argc: parameter of type int .
+  * @argv: parameter of type char *.
+  * @envp: parameter of type char *.
+  * Return: int .
+ */
+int main(int argc, char *argv[], char *envp[])
 {
 	(void)argc, (void)envp;
 	printf("%s\n", argv[0]);
-	return EXIT_SUCCESS;
+	return (EXIT_SUCCESS);
 }
