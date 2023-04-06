258adb947ee51360fa33382fc16d7fe96aa8372e
HazelDaniel <hazeldaniel856@gmail.com>
2023-04-06 09:13:02 +0100

    correction to the previous tasks

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+* _putchar - the function name
+* @c: parameter of type char .
+* Return: int .
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,5 +1,6 @@
 #ifndef _MAIN_
 #define _MAIN_
 #include <stdio.h>
+int _putchar(char c);
 int _atoi(char *s);
 #endif /* _MAIN_ */
