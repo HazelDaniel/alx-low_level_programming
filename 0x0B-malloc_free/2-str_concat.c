1e660949929fec59ae6b9e6cbd18875d7ce9bd88
HazelDaniel <hazeldaniel856@gmail.com>
2023-04-11 19:54:44 +0100

    correction to the task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,22 +11,60 @@ char *str_concat(char *s1, char *s2)
 	int count = 0, i = 0, j = 0;
 	char *temp_str;
 
-	while (*(s1 + count) != '\0' || *(s2 + count) != '\0')
-		count++;
-	temp_str = (char *)malloc(count * sizeof(char) + 1);
-	if (temp_str != NULL)
+	if (s1 == NULL && s2 == NULL)
 	{
-		while (*(s1 + i) != '\0')
-		{
-			*(temp_str + i) = *(s1 + i);
-			i++;
-		}
-		while (*(s2 + j) != '\0')
-		{
-			*(temp_str + i) = *(s2 + j);
-			i++, j++;
-		}
-		*(temp_str + i) = '\0';
+		temp_str = (char *)malloc(sizeof(char));
 	}
+	else if (s1 == NULL && s2 != NULL)
+	{
+		while (*(s2 + count) != '\0')
+			count++;
+		temp_str = (char *)malloc(count * sizeof(char));
+		if (temp_str != NULL)
+		{
+			while (*(s2 + j) != '\0')
+			{
+				*(temp_str + i) = *(s2 + j);
+				i++, j++;
+			}
+			*(temp_str + i) = '\0';
+		}
+	}
+	else if (s1 != NULL && s2 == NULL)
+	{
+		while (*(s1 + count) != '\0')
+			count++;
+		temp_str = (char *)malloc(count * sizeof(char));
+		if (temp_str != NULL)
+		{
+			while (*(s1 + i) != '\0')
+			{
+				*(temp_str + i) = *(s1 + i);
+				i++;
+			}
+			*(temp_str + i) = '\0';
+		}
+	}
+	else
+	{
+		while (*(s1 + count) != '\0' || *(s2 + count) != '\0')
+			count++;
+		temp_str = (char *)malloc(count * sizeof(char));
+		if (temp_str != NULL)
+		{
+			while (*(s1 + i) != '\0')
+			{
+				*(temp_str + i) = *(s1 + i);
+				i++;
+			}
+			while (*(s2 + j) != '\0')
+			{
+				*(temp_str + i) = *(s2 + j);
+				i++, j++;
+			}
+			*(temp_str + i) = '\0';
+		}
+	}
+
 	return (temp_str);
 }
