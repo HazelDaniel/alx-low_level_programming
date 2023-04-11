271950e6be61a977e7e01cb78beabefb04263759
HazelDaniel <hazeldaniel856@gmail.com>
2023-04-11 20:19:50 +0100

    correction to the task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,9 @@
 #include "main.h"
 
+int get_count(char *s1, char *s2);
+int get_count_str(char *s1);
+/*apparently, there is no room for function overloading in c.*/
+
 /**
   * str_concat - the function name
   * @s1: parameter of type char *.
@@ -17,54 +21,61 @@ char *str_concat(char *s1, char *s2)
 	}
 	else if (s1 == NULL && s2 != NULL)
 	{
-		while (*(s2 + count) != '\0')
-			count++;
+		count = get_count_str(s2);
 		temp_str = (char *)malloc(count * sizeof(char));
-		if (temp_str != NULL)
-		{
-			while (*(s2 + j) != '\0')
-			{
-				*(temp_str + i) = *(s2 + j);
-				i++, j++;
-			}
-			*(temp_str + i) = '\0';
-		}
+		if (temp_str == NULL)
+			return (temp_str);
+		while (*(s2 + j) != '\0')
+			*(temp_str + i) = *(s2 + j), i++, j++;
 	}
 	else if (s1 != NULL && s2 == NULL)
 	{
-		while (*(s1 + count) != '\0')
-			count++;
+		count = get_count_str(s1);
 		temp_str = (char *)malloc(count * sizeof(char));
-		if (temp_str != NULL)
-		{
-			while (*(s1 + i) != '\0')
-			{
-				*(temp_str + i) = *(s1 + i);
-				i++;
-			}
-			*(temp_str + i) = '\0';
-		}
+		if (temp_str == NULL)
+			return (temp_str);
+		while (*(s1 + i) != '\0')
+			*(temp_str + i) = *(s1 + i), i++;
 	}
 	else
 	{
-		while (*(s1 + count) != '\0' || *(s2 + count) != '\0')
-			count++;
+		count = get_count(s1, s2);
 		temp_str = (char *)malloc(count * sizeof(char));
 		if (temp_str != NULL)
-		{
-			while (*(s1 + i) != '\0')
-			{
-				*(temp_str + i) = *(s1 + i);
-				i++;
-			}
-			while (*(s2 + j) != '\0')
-			{
-				*(temp_str + i) = *(s2 + j);
-				i++, j++;
-			}
-			*(temp_str + i) = '\0';
-		}
+			return (temp_str);
+		while (*(s1 + i) != '\0')
+			*(temp_str + i) = *(s1 + i), i++;
+		while (*(s2 + j) != '\0')
+			*(temp_str + i) = *(s2 + j), i++, j++;
 	}
-
+	*(temp_str + i) = '\0';
 	return (temp_str);
+}
+
+/**
+  * get_count - the function name
+  * @s1: parameter of type char *.
+  * @s2: parameter of type char *.
+  * Return: char *.
+ */
+int get_count(char *s1, char *s2)
+{
+	int count = 0;
+
+	while (*(s1 + count) != '\0' || *(s2 + count) != '\0')
+		count++;
+	return (count);
+}
+/**
+  * get_count_str - the function name
+  * @s1: parameter of type char *.
+  * Return: char *.
+ */
+int get_count_str(char *s1)
+{
+	int count = 0;
+
+	while (*(s1 + count) != '\0')
+		count++;
+	return (count);
 }
