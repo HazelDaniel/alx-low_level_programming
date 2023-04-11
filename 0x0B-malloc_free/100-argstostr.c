b7eb2b391afc8b0de750f6ce18356ac8df19188a
HazelDaniel <hazeldaniel856@gmail.com>
2023-04-11 22:38:53 +0100

    submission for the task 100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * argstostr - the function name
+  * @ac: parameter of type int .
+  * @av: parameter of type char **.
+  * Return: char *.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i = 0, str_count = 0, s_index;
+	char *temp_str = (char *)malloc(sizeof(char)), *arg;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i++; i < ac; i++)
+	{
+		s_index = 0;
+		arg = *(av + i);
+		while (*(arg + s_index) != '\0')
+		{
+			if (temp_str == NULL)
+				return (temp_str);
+			*(temp_str + str_count) = arg[s_index];
+			s_index++;
+			str_count++;
+			temp_str = realloc(temp_str, str_count + 1);
+		}
+		if (*(arg + (s_index)) == '\0')
+		{
+			str_count++;
+			temp_str = realloc(temp_str, str_count + 1);
+			*(temp_str + (str_count - 1)) = '\n';
+		}
+		if (i == ac - 1)
+			temp_str = realloc(temp_str, str_count - 1);
+	}
+	return (temp_str);
+}
