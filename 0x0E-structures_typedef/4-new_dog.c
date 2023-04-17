241160d3929883b26223277f0227f7dc0a9d3529
HazelDaniel <hazeldaniel856@gmail.com>
2023-04-17 13:40:17 +0100

    correction to the task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,17 +13,33 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog_p = (dog_t *)malloc(sizeof(dog_t));
-	char *name_m = name, *owner_m = owner;
+	int name_i, owner_i, i;
 
-	if (dog_p == NULL)
-		return (dog_p);
-	if (name == NULL)
-		name_m = "(nil)";
-	if (owner == NULL)
-		owner_m = "(nil)";
-
-	dog_p->name = name_m;
+	if (dog_p == NULL || !(name) || !(owner))
+	{
+		free(dog_p);
+		return (NULL);
+	}
+	for (name_i = 0; name[name_i] != '\0'; name_i++)
+		;
+	for (owner_i = 0; owner[owner_i] != '\0'; owner_i++)
+		;
+	dog_p->name = (char *)malloc((name_i + 1) * sizeof(char));
+	dog_p->owner = (char *)malloc((owner_i + 1) * sizeof(char));
+	if (!dog_p->name || !dog_p->owner)
+	{
+		free(dog_p->name);
+		free(dog_p->owner);
+		free(dog_p);
+		return (NULL);
+	}
+	for (i = 0; i < name_i; i++)
+		(dog_p->name)[i] = name[i];
+	(dog_p->name)[i] = '\0';
+	for (i = 0; i < owner_i; i++)
+		(dog_p->owner)[i] = owner[i];
+	(dog_p->owner)[i] = '\0';
 	dog_p->age = age;
-	dog_p->owner = owner_m;
+
 	return (dog_p);
 }
