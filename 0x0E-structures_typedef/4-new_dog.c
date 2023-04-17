6c91a8b48a9652be07003919f66998d0ebd42033
HazelDaniel <hazeldaniel856@gmail.com>
2023-04-17 09:45:15 +0100

    submission for task 2 (4-new_dog.c)

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - a function that creates a
+ * new instance of a dog_t object
+ * @name: a char* parameter
+ * @age: a float parameter
+ * @owner: a char* parameter
+ * Return: a new dot_t object
+ **/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog_p = (dog_t *)malloc(sizeof(dog_t));
+
+	dog_p->name = name;
+	dog_p->age = age;
+	dog_p->owner = owner;
+	return (dog_p);
+}
