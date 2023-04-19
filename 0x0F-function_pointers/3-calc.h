a35c9bc138a3a0d59fea865c891016b41a1acfc7
HazelDaniel <hazeldaniel856@gmail.com>
2023-04-19 16:51:37 +0100

    correction to task 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -3,15 +3,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * struct op - struct op
- * @op: the operator
- * @f: the function associated
- **/
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
 typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-} opt_t;
+} op_t;
 int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,7 +9,7 @@
  **/
 int (*get_op_func(char *s))(int, int)
 {
-	opt_t ops[] = {
+	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
