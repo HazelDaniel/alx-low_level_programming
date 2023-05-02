6e83bd094a6901ac705dc41b6ae4a0b4fe7d3570
HazelDaniel <hazeldaniel856@gmail.com>
2023-05-02 05:53:16 +0100

    correction to the task 102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,7 +8,7 @@
  **/
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *curr = *h, *prev, *fast = *h,
+	listint_t *curr = *h, *prev, *fast = (*h)->next,
 	*slow = *h;
 	unsigned int s_list = 0;
 
