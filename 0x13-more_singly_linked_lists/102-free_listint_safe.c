ec2f2ccf245363e016019b16921c35ef3e7da5f8
HazelDaniel <hazeldaniel856@gmail.com>
2023-05-02 06:06:23 +0100

    correction to the task 102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,13 +8,12 @@
  **/
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *curr = *h, *prev, *fast = (*h)->next,
+	listint_t *curr = *h, *fast = (*h)->next,
 	*slow = *h;
 	unsigned int s_list = 0;
 
 	while (curr != NULL)
 	{
-		prev = curr;
 		curr = curr->next;
 		if (fast != NULL && fast->next != NULL)
 		{
@@ -27,7 +26,6 @@ size_t free_listint_safe(listint_t **h)
 			s_list += 2;
 			exit(98);
 		}
-		free(prev);
 	}
 
 	*h = NULL;
