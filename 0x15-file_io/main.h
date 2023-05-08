c8cda4f41ea82be308c575bfee7d715f2c87eb06
HazelDaniel <hazeldaniel856@gmail.com>
2023-05-08 14:06:29 +0100

    added README, test files, object files, and header file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,9 @@
+#ifndef ___MAIN_
+#define ___MAIN_
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+#endif/*___MAIN_*/
