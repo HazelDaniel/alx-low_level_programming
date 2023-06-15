947f462a110fab8e0ad7f69e4ad75fcd258ff42c
HazelDaniel <hazeldaniel856@gmail.com>
2023-06-15 16:23:50 +0100

    re-submission for the task 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,17 +2,18 @@
 
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *current = head, *prev;
+	dlistint_t *current = head;
 
 	while (current)
 	{
+		if (!current->next)
+		{
+			free(current->prev), free(current), head = NULL;
+			return;
+		}
 		free(current->prev);
-		prev = current;
 		current = current->next;
 	}
-	free(prev);
-	if (current)
-		free(current->prev);
 	free(current);
 	head = NULL;
 }
