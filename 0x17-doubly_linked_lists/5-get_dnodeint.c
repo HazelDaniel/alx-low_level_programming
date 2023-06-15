1103d02d84689407adc28f665b5067d8be4737b3
HazelDaniel <hazeldaniel856@gmail.com>
2023-06-15 16:28:34 +0100

    submission for the task 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	dlistint_t *current = head;
+	
+	while (current)
+	{
+		current = current->next;
+		count++;
+		if (count == index)
+			return current;
+	}
+	return (NULL);
+}
