6356165842716d5034f969276fb3ba78fafbb544
HazelDaniel <hazeldaniel856@gmail.com>
2023-06-19 14:06:42 +0100

    half submission for the task 100

---
+++ 0x18-dynamic_libraries/list.h
@@ -0,0 +1,8 @@
+#ifndef ___LIST_
+#define ___LIST_
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+#endif/*___LIST_*/
---
+++ 0x18-dynamic_libraries/operation.c
@@ -0,0 +1,56 @@
+#include "list.h"
+
+/**
+  * add - the function name
+  * @a: parameter of type int .
+  * @b: parameter of type int .
+  * Return: int .
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * sub - the function name
+  * @a: parameter of type int .
+  * @b: parameter of type int .
+  * Return: int .
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * mul - the function name
+  * @a: parameter of type int .
+  * @b: parameter of type int .
+  * Return: int .
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * div - the function name
+  * @a: parameter of type int .
+  * @b: parameter of type int .
+  * Return: int .
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  * mod - the function name
+  * @a: parameter of type int .
+  * @b: parameter of type int .
+  * Return: int .
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
