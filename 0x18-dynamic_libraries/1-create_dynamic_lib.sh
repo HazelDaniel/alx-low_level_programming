ce4ab6d7656aa2cf5cf0908cf5667bfb1f9113e3
HazelDaniel <hazeldaniel856@gmail.com>
2023-06-19 13:56:17 +0100

    submission for the task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-for i in $(cat libfiles); do
-	temp=$(echo $i | tr -dc '/' | wc -c)
-	res=$(echo "$i" | cut -d '/' -f $(($temp + 1)))
-	`gcc -fPIC -c $res`
+for i in $(ls *.c); do
+	`gcc -fPIC -c $i`
 done
 gcc -shared -o libdynamic.so *.o
