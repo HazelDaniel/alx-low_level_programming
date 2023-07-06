fdc7d03793c5ee139e839bc23b7d866d1441c655
HazelDaniel <hazeldaniel856@gmail.com>
2023-07-06 20:58:37 +0100

    removed the slot for sentinel values, adjusted the size of the hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t));
-	unsigned long int i = 0;
 
 	if (!table || !size)
 	{
@@ -17,17 +16,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	table->size = size;
-	table->array = (hash_node_t **)calloc(size + 1, sizeof(hash_node_t *));
+	table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
 
 	if (!table->array)
 	{
 		free(table);
 		return (NULL);
-	}
-
-	for (; i <= size; )
-	{
-		(table->array)[i++] = NULL;
 	}
 
 	return (table);
