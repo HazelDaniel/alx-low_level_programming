1b1c20babe198d8eda5de66697a336d4d9628a06
HazelDaniel <hazeldaniel856@gmail.com>
2023-12-11 06:37:04 +0100

    task-0: linear search on a possibly unsorted array

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - a function that performs a linear search
+ * on an array of integers
+ * @array: the array in question
+ * @size: the length of the array
+ * @value: the target value
+ * Return: the first index with the first occurrence of the target
+ * value or -1
+ **/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t ind = 0;
+
+	while (ind < size) {
+		printf("Value checked array[%u] = [%d]\n", (unsigned int)ind, array[ind]);
+		if (array[ind] == value)
+			return (ind);
+		ind++;
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,5 @@
+#ifndef ___SEARCH_ALGOS_
+#define ___SEARCH_ALGOS_
+#include <stdlib.h>
+int linear_search(int *array, size_t size, int value);
+#endif/*___SEARCH_ALGOS_*/
