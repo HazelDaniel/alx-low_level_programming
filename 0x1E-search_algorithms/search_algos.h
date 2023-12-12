e70492d7b5422ba7cb08dbacdfd031e2dc72dbc2
HazelDaniel <hazeldaniel856@gmail.com>
2023-12-12 04:41:24 +0100

    updated the header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,4 +3,6 @@
 #include <stdlib.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 #endif/*___SEARCH_ALGOS_*/
