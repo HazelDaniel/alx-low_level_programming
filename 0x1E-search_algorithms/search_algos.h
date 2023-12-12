2e7b0866a3bafd8a649ccdd1469339bdc25fc83d
HazelDaniel <hazeldaniel856@gmail.com>
2023-12-12 11:42:22 +0100

    update made to the header file to comply to the requirements

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,10 +1,14 @@
 #ifndef ___SEARCH_ALGOS_
 #define ___SEARCH_ALGOS_
 #include <stdlib.h>
+
+/* CORE FUNCTIONS */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
-extern size_t min(size_t a, size_t b);
+int advanced_binary(int *array, size_t size, int value);
+
+/* UTILITY FUNCTIONS */
 #endif/*___SEARCH_ALGOS_*/
