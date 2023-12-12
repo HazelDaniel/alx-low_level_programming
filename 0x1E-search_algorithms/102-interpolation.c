c9b5dd1434f710088f4f38e163e397bbc0d859a8
HazelDaniel <hazeldaniel856@gmail.com>
2023-12-12 04:48:27 +0100

    task-102: fixed betty issues

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -18,32 +18,28 @@ int interpolation_search(int *array, size_t size, int value)
 {
 	size_t mid, low = 0, high = size - 1, tmp;
 
-	(void)tmp;
 	if (!array)
 		return (-1);
 
-	while ((array[high] != array[low]) && (value <= array[high]) && (value >= array[low]))
+	while ((array[high] != array[low]) && (value <= array[high])
+		&& (value >= array[low]))
 	{
-		mid = low + ((value - array[low]) * (high - low) / (array[high] - array[low]));
+		mid = low + ((value - array[low]) * (high - low)
+			/ (array[high] - array[low]));
 		printf("Value checked array[%u] = [%d]\n", (unsigned int)mid, array[mid]);
 		if (array[mid] == value)
-		{
 			return (mid);
-		}
 		else if (array[mid] > value)
-		{
 			high = mid - 1;
-		}
 		else
-		{
 			low = mid + 1;
-		}
 	}
-	if (tmp = mid, array[mid] == value ||
-		(tmp = low, array[low] == value))
+	tmp = array[mid] == value ? mid : array[low] == value ? low : 0;
+	if (array[mid] == value || array[low] == value)
 		return (tmp);
 
-	mid = low + ((value - array[low]) * (high - low) / (array[high] - array[low]));
+	mid = low + ((value - array[low]) * (high - low)
+		/ (array[high] - array[low]));
 	printf("Value checked array[%u] is out of range\n", (unsigned int)mid);
 	return (-1);
 }
