da3ce092a39f9697b890d24ca189d2225f2f5bd1
HazelDaniel <hazeldaniel856@gmail.com>
2023-12-12 04:41:06 +0100

    function implementation for a weird algorithm that runs in O(log(log(n))) at its best and O(n) at its worst - interpolation sort

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+
+/**
+ * interpolation_search - a function that performs an operation
+ * on a presumably sorted array similar to binary search
+ * but partitions the array uniformly based on the difference
+ * between the elements, and their distance. it's more effecient
+ * than binary search when the elements in the array are unique
+ * and uniformly distributed
+ * @array: the array of sorted arrays
+ * @size: the length of the array
+ * @value: the target value
+ * Return: the first occurrence of the target or -1
+ **/
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t mid, low = 0, high = size - 1, tmp;
+
+	(void)tmp;
+	if (!array)
+		return (-1);
+
+	while ((array[high] != array[low]) && (value <= array[high]) && (value >= array[low]))
+	{
+		mid = low + ((value - array[low]) * (high - low) / (array[high] - array[low]));
+		printf("Value checked array[%u] = [%d]\n", (unsigned int)mid, array[mid]);
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] > value)
+		{
+			high = mid - 1;
+		}
+		else
+		{
+			low = mid + 1;
+		}
+	}
+	if (tmp = mid, array[mid] == value ||
+		(tmp = low, array[low] == value))
+		return (tmp);
+
+	mid = low + ((value - array[low]) * (high - low) / (array[high] - array[low]));
+	printf("Value checked array[%u] is out of range\n", (unsigned int)mid);
+	return (-1);
+}
