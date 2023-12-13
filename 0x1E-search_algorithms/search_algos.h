8dfbf9725720dcf3ef646872e26100400fafac45
HazelDaniel <hazeldaniel856@gmail.com>
2023-12-13 18:07:00 +0100

    added the header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -21,6 +21,22 @@ typedef struct skiplist_s
 	struct skiplist_s *express;
 } skiplist_t;
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
 
 /* CORE FUNCTIONS */
 int linear_search(int *array, size_t size, int value);
@@ -29,6 +45,7 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
 
