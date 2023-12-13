90746e6389e9f19727cad42621c0993fe79013a7
HazelDaniel <hazeldaniel856@gmail.com>
2023-12-13 08:46:48 +0100

    task-100: implementation of jump search algorithm that runs in O(sqrt(n)) in the average case and O(n) in the worst. which is still better than linear search. fixed another edge case BTW

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -15,6 +15,37 @@ size_t min(size_t x, size_t y)
 		return (x);
 	return (y);
 }
+
+/**
+ * print_range - prints a sequence of integers
+ * at indices in a specified range
+ * @start: the starting index
+ * @end: the ending index
+ * @size: the length of the sequence array
+ * @array: the sequence array
+ * Return: void
+ **/
+void print_range(size_t start, size_t end, size_t size, int *array)
+{
+	size_t l;
+
+	printf("Value found between indexes [%u] and [%u]\n",
+		(unsigned int)start, (unsigned int)end);
+	for (l = start; l < size && l <= end; l++)
+		printf("Value checked array[%u] = [%d]\n", (unsigned int)l, array[l]);
+}
+
+/**
+ * print_value_checked - prints a value given its index
+ * @index: the provided index
+ * @array: the array containing the value
+ * Return: void
+ **/
+void print_value_checked(int *array, size_t index)
+{
+	printf("Value checked array[%u] = [%d]\n", (unsigned int)index, array[index]);
+}
+
 /**
  * jump_search - a function that implements the jump search
  * algorithm on a sorted array and prints the sequence for
@@ -26,55 +57,43 @@ size_t min(size_t x, size_t y)
  **/
 int jump_search(int *array, size_t size, int value)
 {
-	size_t m = (size_t)sqrt(size), k = 1, i = 0, y = 0, x, j, tmp = 0, l = 0;
+	size_t m = (size_t)sqrt(size), k = 1, i = 0, y = 0, x, j, tmp = 0;
+	int v;
 
 	if (!array)
 		return (-1);
-	while (i < size)
+	while (i < size && y < size)
 	{
-		if (y < size)
-			printf("Value checked array[%u] = [%d]\n", (unsigned int)y, array[y]);
-		y = min(m * k, size);
-		if (y == size)
-			return (-1);
+		print_value_checked(array, y), y = min(m * k, size);
 		if (y == size - 1)
-		{
-			printf("Value checked array[%u] = [%d]\n", (unsigned int)y, array[y]);
-			tmp = y, y = m * (k + 1);
-			printf("Value found between indexes [%u] and [%u]\n", (unsigned int)tmp, (unsigned int)y);
-			for (l = tmp; l < size && l <= y; l++)
-				printf("Value checked array[%u] = [%d]\n", (unsigned int)l, array[l]);
-		}
-
+			print_value_checked(array, y), tmp = y, y = m * (k + 1),
+				print_range(tmp, y, size, array);
 		if (array[y] > value)
 		{
 			x = m * (k - 1), j = y;
-			while (j >= x)
+			while (j--, j >= x)
 			{
 				if (array[j] == value)
 				{
-					printf("Value found between indexes [%u] and [%u]\n", (unsigned int)x, (unsigned int)y);
-					for (l = x; l <= size - 1 && l <= j; l++)
-						printf("Value checked array[%u] = [%d]\n", (unsigned int)l, array[l]);
+					print_range(x, y, size, array);
 					return (j);
 				}
 				else if (array[j] < value)
 				{
 					return (-1);
 				}
-				j--;
 			}
 		}
 		else if (array[y] == value)
 		{
-			printf("Value found between indexes [%u] and [%u]\n", (unsigned int)tmp, (unsigned int)y);
-			for (l = tmp; l <= size - 1 && l <= y; l++)
-				printf("Value checked array[%u] = [%d]\n", (unsigned int)l, array[l]);
+			print_range(tmp, y, size, array);
 			return (y);
 		}
-		i++, k++;
-		tmp = y;
+		i++, k++, tmp = y, v = y;
 	}
-
+	if (v >= (int)size)
+		for (v = size; v >= 0; --v)
+			if (array[v] == value)
+				return (v);
 	return (-1);
 }
